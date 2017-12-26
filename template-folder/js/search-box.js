865b9a74afc5c9b366fd7472d9849d74f5449a64
Marnel Mangrubang <delrosam@gmail.com>
2017-12-26 09:09:04 -0800

    Cards modules
    Created the cards modules

--- template-folder/js/search-box.js
+++ template-folder/js/search-box.js
@@ -65,6 +65,41 @@
         value: "Basic HTML 5 Template",
         label: "Basic HTML 5 Template",
         anchor: "/template-folder/basic-html5.html"
+      },
+      {
+        value: "Cards 1 - 3x1",
+        label: "Cards 1 - 3x1 Template",
+        anchor: "/cards/cards.html#cd-1"
+      },
+      {
+        value: "Cards 1.2 - 3x1",
+        label: "Cards 1.2 - 3x1 Template",
+        anchor: "/cards/cards.html#cd-1-2"
+      },      ,
+      {
+        value: "Cards 2 - 3x1 Fullsize",
+        label: "Cards 2 - 3x1 Fullsize Template",
+        anchor: "/cards/cards.html#cd-2"
+      },
+      {
+        value: "Cards 2.1 - 3x1 Fullsize Initially Hidden Overlay",
+        label: "Cards 2.1 - 3x1 Fullsize Initially Hidden Overlay",
+        anchor: "/cards/cards.html#cd-2-1"
+      },
+      {
+        value: "Cards 2.2 - 3x1 Fullsize Just Title",
+        label: "Cards 2.2 - 3x1 Fullsize Just Title",
+        anchor: "/cards/cards.html#cd-2-2"
+      },
+      {
+        value: "Cards 2.3 - 3x1 Fullsize Overlay Swap",
+        label: "Cards 2.3 - 3x1 Fullsize Overlay Swap",
+        anchor: "/cards/cards.html#cd-2-3"
+      },
+      {
+        value: "Cards 3 - 3x1 Fullsize Inverse",
+        label: "Cards 3 - 3x1 Fullsize Inverse",
+        anchor: "/cards/cards.html#cd-3"
       }
     ];
 
